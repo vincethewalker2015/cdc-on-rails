75148c2d6f7218c4a1308c83bc2ae395f6ddadb6
Vincent <vincethewalker@yahoo.co.uk>
2020-05-08 15:36:52 +0100

    Completed contact mailer set up

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,34 @@
 class ContactsController < ApplicationController
   
-  def new
-    @contact = Contact.new
-  end
+  # def new
+  #   @contact = Contact.new THIS IS PLACED IN THE PAGES CONTROLLER
+  # end
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     
     if @contact.save
-      ContactsMailer.general_message(@contact).deliver
-      render :thanks
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:message]
+      ContactMailer.contact_email(name, email, body).deliver
+      flash[:success] = "Message sent"
+      puts "Successful"
+      redirect_to root_path
     else
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+      puts "Unsuccessful"
       render :new
     end
   end
   
-  def thanks
+  def show
+  end
+    
+  private 
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
     
 end
